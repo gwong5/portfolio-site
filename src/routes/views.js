f44cd85e05b3c195810fe9459074463c94cb9449
Greg <kousagi1012@yahoo.com>
2017-07-07 12:03:11 -0700

    adds more views

--- src/routes/views.js
+++ src/routes/views.js
@@ -5,4 +5,12 @@ router.get('/', (request, response) => {
   response.render('index')
 })
 
+router.get('/profile', (request, response) => {
+  response.render('profile')
+})
+
+router.get('/contact', (request, response) => {
+  response.render('contact')
+})
+
 module.exports = router
